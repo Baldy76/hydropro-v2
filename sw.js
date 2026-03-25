bc7fd42ac516adbbad3930b03367acb098e0924d
Baldy76 <rgleech76@gmail.com>
2026-03-25 09:59:55 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'hydro-pro-v59';
+const ASSETS = [
+    './index.html',
+    './styles.css',
+    './app.js',
+    './Logo.png',
+    'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
